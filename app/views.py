16aa6a9471e055ea645aec0423b1d8bab68954ec
chynajake <chyna.jake25@gmail.com>
2017-07-01 22:57:19 +0600

    [Development]-(Templates)-Dynamic template shows all news

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'app/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'app/post_list.html', {'posts': posts})
