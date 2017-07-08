f2ceb1a8fee40154becaeb126433f0bb3a870611
chynajake <chyna.jake25@gmail.com>
2017-07-09 00:20:14 +0600

    [Development]-(Cache/Templates)-Added more functionality filter for the post seen or not seen by the user(top button third from right)

--- app/views.py
+++ app/views.py
@@ -4,12 +4,11 @@ from .forms import PostForm
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.core.cache import cache
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
-@login_required
-def post_list(request):
-    posts = Post.objects.all()
+def cached_posts(request):
     user_id = request.user.pk
     visited_pages = cache.get(user_id)
     if visited_pages is None:
@@ -18,12 +17,30 @@ def post_list(request):
     pages = []
     print(visited_pages)
     print(visited_pages.split(' '))
-    for a in visited_pages:
+    for a in visited_pages.split(' '):
         if a is not "" and a is not " ":
             pages.append(int(a))
     print(pages)
+    return pages
 
+def paginated_posts(request ,all_posts):
+    page = request.GET.get('page', 1)
 
+    paginator = Paginator(all_posts, 4)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return posts
+
+@login_required
+def post_list(request):
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = paginated_posts(request, post_list)
+    pages = cached_posts(request)
 
     return render(request, 'app/post_list.html', {'posts': posts, 'pages': pages})
 
@@ -81,10 +98,26 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail', pk=pk)
+    return redirect('post_list')
 
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect(post_list)
+
+@login_required
+def post_unchecked(request):
+
+    all_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    cached_pages = cached_posts(request)
+    posts = []
+    unchecked_posts = []
+
+    for post in all_posts:
+        if post.pk not in cached_pages:
+            unchecked_posts.append(post)
+    unchecked_posts = paginated_posts(request, unchecked_posts)
+    unchecked = True
+
+    return render(request, 'app/post_unchecked_list.html', {'posts': unchecked_posts, 'pages': cached_pages, 'unchecked': unchecked})
